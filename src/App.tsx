6ee3f58ed8ef5c5f53cdeda5f022de45c7145cf2
ngel rojas <aomarrojasp@gmail.com>
2024-01-03 22:03:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-6xl font-bold underline text-red-600">
+            simple react app with typescript and tailwind css sample
+        </h1>
+    </>
+  )
+}
+
+export default App
