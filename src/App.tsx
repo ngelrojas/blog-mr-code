fb49434ca26e14c1c7cd5b68919dd9540c6005ea
ngel rojas <aomarrojasp@gmail.com>
2024-01-03 23:58:27 -0300

    branch test home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
+import NavBar from "./components/navBar";
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-full">
+        <NavBar/>
         <h1 className="text-6xl font-bold underline text-red-600">
             simple react app with typescript and tailwind css sample
         </h1>
-    </>
+    </div>
   )
 }
 
