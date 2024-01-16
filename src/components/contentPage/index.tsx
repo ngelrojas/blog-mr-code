e5879e19a08dc58a9fd873630f3bd5d3dd40078a
ngel rojas <aomarrojasp@gmail.com>
2024-01-15 21:40:11 -0300

    add: adding modal window and refactor code

--- src/components/contentPage/index.tsx
+++ src/components/contentPage/index.tsx
@@ -1,9 +1,10 @@
-import Card from "../card";
+import ListCard from "../listCard";
+
 export default function ContentPage(){
     return (
         <main>
             <div className="mx-auto max-w-5xl py-10 sm:px-10 lg:px-12">
-                <Card/>
+                <ListCard />
             </div>
         </main>
     )
