bab1c60db610c8353535122e7a673606dba76d37
Jorge Lopez <lopez.cs.jorge@gmail.com>
2024-02-14 18:42:05 -0300

    add:se adiciono un test

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,14 +1,11 @@
-
 const Dashboard = () => {
-    return (
-        <div className="min-h-full">
-            <div className="pt-28">
-
-                <h1>Dashboard</h1>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="min-h-full">
+      <div className="pt-28">
+        <h1>Dashboard jorge</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
