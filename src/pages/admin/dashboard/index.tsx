eb58291fbacbc56699fedb7122808b659280fca7
ngel rojas <aomarrojasp@gmail.com>
2024-02-14 18:33:41 -0300

    add: adding template for admin page

---
+++ src/pages/admin/dashboard/index.tsx
@@ -0,0 +1,14 @@
+
+const Dashboard = () => {
+    return (
+        <div className="min-h-full">
+            <div className="pt-28">
+
+                <h1>Dashboard</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Dashboard;
--- src/routes/webSite/index.tsx
+++ src/routes/webSite/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from "../../pages/home";
 import Category from "../../pages/category";
 import Description from "../../pages/description";
+import Dashboard from "../../pages/admin/dashboard";
 
 export const webSiteRoutes = createBrowserRouter([
     {
@@ -20,4 +21,12 @@ export const webSiteRoutes = createBrowserRouter([
         path: 'category/:description/:slug',
         element: <Description />,
     },
+    {
+        path: 'admin/dashboard',
+        element: <Dashboard />
+    },
+    {
+        path: 'admin',
+        element: <Dashboard />
+    }
 ])
